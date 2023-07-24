993dcbf57e7587e5359106df03dbd35709e5de91
sbx_user1051 <sbx_user1051@169.254.69.209>
2023-07-24 17:58:35 +0000

    Mon Jul 24 17:58:35 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,24 @@
+def count_instances(skus, values_list):
+
+    for value in values_list:
+        print(skus.count(value))
+        return skus.count(value)
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+    allow_list: ['A', 'B', 'C', 'D']
+
+    if len(skus) < 1 or not skus.isalpha():
+        return -1
+
+    count_instances(skus, allow_list)
+
+checkout()
+
+
+
+
+
