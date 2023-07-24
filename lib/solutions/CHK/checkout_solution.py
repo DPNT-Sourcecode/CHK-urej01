a4bfdc04153478e2a87ba7727943f67ce4691dae
sbx_user1051 <sbx_user1051@169.254.69.209>
2023-07-24 18:04:36 +0000

    Mon Jul 24 18:04:36 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,23 +1,25 @@
+def get_value(sku, count):
+
+
 def count_instances(skus, values_list):
     print(values_list)
     for value in values_list:
-        print(value)
-        print(skus.count(value))
+        get_value(value, skus.count(value))
         # return skus.count(value)
-
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
 
-    allow_list = ['A', 'B', 'C', 'D']
+    allow_list = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
 
     if len(skus) < 1 or not skus.isalpha():
         return -1
 
-    count_instances(skus, allow_list)
+    count_instances(skus, allow_list.keys())
 
 checkout('AAABBBCDE')
+
 
 
 
